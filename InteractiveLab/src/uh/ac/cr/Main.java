d34cfeb88b12fb31b9a5108d80bfd2237cc9fc67
Rseg0795 <85773375+Rseg0795@users.noreply.github.com>
2021-06-18 23:40:40 -0600

    Add files via upload

---
+++ InteractiveLab/src/uh/ac/cr/Main.java
@@ -0,0 +1,7 @@
+package uh.ac.cr;
+
+public class Main {
+    public static void main(String[] args) {
+        Tarea3 Tarea = new Tarea3;
+        System.out.println(Tarea);
+}}
---
+++ InteractiveLab/src/uh/ac/cr/Tarea3.java
@@ -0,0 +1,19 @@
+package uh.ac.cr;
+
+import java.util.Scanner;
+
+public class Tarea3 {
+
+   public void String (){
+    int i = 0;
+       Scanner usuario = new Scanner(System.in);
+            System.out.print( "Cuantas veces desea repetir la oracion: " );
+       if (i < usuario) {
+           do {
+               System.out.println("Hola mundo");
+               i++;
+           } while (i = usuario);
+       }
+   }
+}
+
